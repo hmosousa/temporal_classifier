77750208e8fd870b71b6d2a5b81b4a19cb7dfa4d
Hugo Sousa <hmosousa@gmail.com>
2025-01-09 14:20:30 +0000

    Add results for model trained on synthetic data

--- scripts/model/eval/run.sh
+++ scripts/model/eval/run.sh
@@ -6,11 +6,7 @@ python scripts/model/eval/point.py -m majority -d temporal_questions -c raw
 python scripts/model/eval/point.py -m hugosousa/smol-135-tq -d temporal_questions -c raw -b 1024
 python scripts/model/eval/point.py -m hugosousa/smol-135-tq-augment -d temporal_questions -c raw -b 1024
 python scripts/model/eval/point.py -m hugosousa/smol-135-tq-closure -d temporal_questions -c raw -b 1024
-python scripts/model/eval/point.py -m hugosousa/smol-135-tq-synthetic -d temporal_questions -c raw -b 1024
-
-python scripts/model/eval/point.py -m hugosousa/smol-360-tq -d temporal_questions -c raw -b 1024
-
-python scripts/model/eval/point.py -m hugosousa/SmolLM2-1.7B-TemporalQuestions -d temporal_questions -c raw -b 256
+python scripts/model/eval/point.py -m hugosousa/smol-135-tq-synthetic -d temporal_questions -c raw -b 128
 
 ## Timeset
 python scripts/model/eval/point.py -m random -d timeset
@@ -18,7 +14,7 @@ python scripts/model/eval/point.py -m majority -d timeset
 python scripts/model/eval/point.py -m hugosousa/smol-135-tq -d timeset -b 1024
 python scripts/model/eval/point.py -m hugosousa/smol-135-tq-augment -d timeset -b 1024
 python scripts/model/eval/point.py -m hugosousa/smol-135-tq-closure -d timeset -b 1024
-python scripts/model/eval/point.py -m hugosousa/smol-135-tq-synthetic -d timeset -b 1024
+python scripts/model/eval/point.py -m hugosousa/smol-135-tq-synthetic -d timeset -b 128
 
 ## MATRES
 python scripts/model/eval/point.py -m random -d matres
@@ -26,7 +22,7 @@ python scripts/model/eval/point.py -m majority -d matres
 python scripts/model/eval/point.py -m hugosousa/smol-135-tq -d matres -b 1024
 python scripts/model/eval/point.py -m hugosousa/smol-135-tq-augment -d matres -b 1024
 python scripts/model/eval/point.py -m hugosousa/smol-135-tq-closure -d matres -b 1024
-python scripts/model/eval/point.py -m hugosousa/smol-135-tq-synthetic -d matres -b 1024
+python scripts/model/eval/point.py -m hugosousa/smol-135-tq-synthetic -d matres -b 128
 
 # Interval eval
 
--- scripts/model/eval/run_loss.sh
+++ scripts/model/eval/run_loss.sh
@@ -7,47 +7,39 @@ python scripts/model/eval/point.py -m random -d temporal_questions -c raw
 python scripts/model/eval/point.py -m majority -d temporal_questions -c raw
 python scripts/model/eval/point.py -m hugosousa/smol-135-tq -r 07efc5dbe75d2b5c7108832ca1b19619ed9200c0 -d temporal_questions -c raw -b 1024
 python scripts/model/eval/point.py -m hugosousa/smol-135-tq-augment -r e5219f261ff7b5809428a4e53b9946a5442474f8 -d temporal_questions -c raw -b 1024
-python scripts/model/eval/point.py -m hugosousa/smol-135-tq-closure -d temporal_questions -c raw -b 1024
-python scripts/model/eval/point.py -m hugosousa/smol-135-tq-synthetic -d temporal_questions -c raw -b 1024
-
-python scripts/model/eval/point.py -m hugosousa/smol-360-tq -d temporal_questions -c raw -b 1024
-
-python scripts/model/eval/point.py -m hugosousa/SmolLM2-1.7B-TemporalQuestions -d temporal_questions -c raw -b 256
+python scripts/model/eval/point.py -m hugosousa/smol-135-tq-closure -r 55b23176775dec273e2705b15e412f05a021a5db -d temporal_questions -c raw -b 1024
+python scripts/model/eval/point.py -m hugosousa/smol-135-tq-synthetic -r ad71005a0a3c3088079ec2103e62a07c7cf86f9e -d temporal_questions -c raw -b 1024
 
 ## Timeset
 python scripts/model/eval/point.py -m random -d timeset
 python scripts/model/eval/point.py -m majority -d timeset
 python scripts/model/eval/point.py -m hugosousa/smol-135-tq -r 07efc5dbe75d2b5c7108832ca1b19619ed9200c0 -d timeset -b 1024
 python scripts/model/eval/point.py -m hugosousa/smol-135-tq-augment -r e5219f261ff7b5809428a4e53b9946a5442474f8 -d timeset -b 1024
-python scripts/model/eval/point.py -m hugosousa/smol-135-tq-closure -d timeset -b 1024
-python scripts/model/eval/point.py -m hugosousa/smol-135-tq-synthetic -d timeset -b 1024
+python scripts/model/eval/point.py -m hugosousa/smol-135-tq-closure -r 55b23176775dec273e2705b15e412f05a021a5db -d timeset -b 1024
+python scripts/model/eval/point.py -m hugosousa/smol-135-tq-synthetic -r ad71005a0a3c3088079ec2103e62a07c7cf86f9e -d timeset -b 1024
 
 ## MATRES
 python scripts/model/eval/point.py -m random -d matres
 python scripts/model/eval/point.py -m majority -d matres
 python scripts/model/eval/point.py -m hugosousa/smol-135-tq -r 07efc5dbe75d2b5c7108832ca1b19619ed9200c0 -d matres -b 1024
 python scripts/model/eval/point.py -m hugosousa/smol-135-tq-augment -r e5219f261ff7b5809428a4e53b9946a5442474f8 -d matres -b 1024
-python scripts/model/eval/point.py -m hugosousa/smol-135-tq-closure -d matres -b 1024
-python scripts/model/eval/point.py -m hugosousa/smol-135-tq-synthetic -d matres -b 1024
+python scripts/model/eval/point.py -m hugosousa/smol-135-tq-closure -r 55b23176775dec273e2705b15e412f05a021a5db -d matres -b 1024
+python scripts/model/eval/point.py -m hugosousa/smol-135-tq-synthetic -r ad71005a0a3c3088079ec2103e62a07c7cf86f9e -d matres -b 1024
 
 # Interval eval
 
 ## TempEval-3
 python scripts/model/eval/interval.py -m random -d tempeval_3
 python scripts/model/eval/interval.py -m majority -d tempeval_3
-python scripts/model/eval/interval.py -m hugosousa/smol-135-tq -d tempeval_3
-python scripts/model/eval/interval.py -m hugosousa/smol-135-tq-augment -d tempeval_3
-python scripts/model/eval/interval.py -m hugosousa/smol-135-tq-closure -d tempeval_3
-python scripts/model/eval/interval.py -m hugosousa/smol-135-tq-synthetic -d tempeval_3
-
-python scripts/model/eval/interval.py -m hugosousa/SmolLM2-1.7B-TemporalQuestions -d tempeval_3
+python scripts/model/eval/interval.py -m hugosousa/smol-135-tq -r 07efc5dbe75d2b5c7108832ca1b19619ed9200c0 -d tempeval_3
+python scripts/model/eval/interval.py -m hugosousa/smol-135-tq-augment -r e5219f261ff7b5809428a4e53b9946a5442474f8 -d tempeval_3
+python scripts/model/eval/interval.py -m hugosousa/smol-135-tq-closure -r 55b23176775dec273e2705b15e412f05a021a5db -d tempeval_3
+python scripts/model/eval/interval.py -m hugosousa/smol-135-tq-synthetic -r ad71005a0a3c3088079ec2103e62a07c7cf86f9e -d tempeval_3
 
 ## TDDiscourse
 python scripts/model/eval/interval.py -m random -d tddiscourse
 python scripts/model/eval/interval.py -m majority -d tddiscourse
-python scripts/model/eval/interval.py -m hugosousa/smol-135-tq -d tddiscourse
-python scripts/model/eval/interval.py -m hugosousa/smol-135-tq-augment -d tddiscourse
-python scripts/model/eval/interval.py -m hugosousa/smol-135-tq-closure -d tddiscourse
-python scripts/model/eval/interval.py -m hugosousa/smol-135-tq-synthetic -d tddiscourse
-
-python scripts/model/eval/interval.py -m hugosousa/SmolLM2-1.7B-TemporalQuestions -d tddiscourse
+python scripts/model/eval/interval.py -m hugosousa/smol-135-tq -r 07efc5dbe75d2b5c7108832ca1b19619ed9200c0 -d tddiscourse
+python scripts/model/eval/interval.py -m hugosousa/smol-135-tq-augment -r e5219f261ff7b5809428a4e53b9946a5442474f8 -d tddiscourse
+python scripts/model/eval/interval.py -m hugosousa/smol-135-tq-closure -r 55b23176775dec273e2705b15e412f05a021a5db -d tddiscourse
+python scripts/model/eval/interval.py -m hugosousa/smol-135-tq-synthetic -r ad71005a0a3c3088079ec2103e62a07c7cf86f9e -d tddiscourse
