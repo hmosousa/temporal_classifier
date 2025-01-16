ae851be3e91cc83c971e3163ee4a9d52c9a5fd33
Hugo Sousa <hmosousa@gmail.com>
2025-01-16 10:49:08 +0000

    Fix tddiscourse

--- src/data/interval/tddiscourse.py
+++ src/data/interval/tddiscourse.py
@@ -6,17 +6,64 @@ import tieval.datasets
 from src.data.utils import get_tlink_context
 
 
+# Train, valid, test split from https://github.com/aakanksha19/TDDiscourse
+TRAIN_DOCS = [
+    "APW19980227.0468",
+    "APW19980213.1320",
+    "CNN19980222.1130.0084",
+    "ABC19980108.1830.0711",
+    "PRI19980205.2000.1890",
+    "PRI19980205.2000.1998",
+    "ABC19980120.1830.0957",
+    "APW19980213.1380",
+    "ea980120.1830.0071",
+    "CNN19980227.2130.0067",
+    "ea980120.1830.0456",
+    "ABC19980304.1830.1636",
+    "APW19980219.0476",
+    "APW19980213.1310",
+    "PRI19980213.2000.0313",
+    "AP900816-0139",
+    "PRI19980121.2000.2591",
+    "NYT19980206.0466",
+    "NYT19980206.0460",
+    "AP900815-0044",
+    "ABC19980114.1830.0611",
+]
+
+VALID_DOCS = [
+    "APW19980227.0487",
+    "PRI19980216.2000.0170",
+    "ed980111.1130.0089",
+    "CNN19980223.1130.0960",
+]
+
+TEST_DOCS = [
+    "APW19980418.0210",
+    "CNN19980213.2130.0155",
+    "APW19980227.0494",
+    "CNN19980126.1600.1104",
+    "APW19980308.0201",
+    "NYT19980402.0453",
+    "APW19980227.0489",
+    "PRI19980306.2000.1675",
+    "PRI19980115.2000.0186",
+]
+
+
 def load_tddiscourse(
-    split: Literal["train", "test"],
+    split: Literal["train", "valid", "test"],
     **kwargs,
 ) -> datasets.Dataset:
     """Load TDDiscourse dataset."""
     corpus = tieval.datasets.read("tddiscourse")
 
     if split == "train":
-        docs = corpus.train
+        docs = [doc for doc in corpus.train if doc.name in TRAIN_DOCS]
+    elif split == "valid":
+        docs = [doc for doc in corpus.train if doc.name in VALID_DOCS]
     elif split == "test":
-        docs = corpus.test
+        docs = [doc for doc in corpus.test if doc.name in TEST_DOCS]
     else:
         raise ValueError(f"Invalid split: {split}")
 
--- tests/test_data/test_tddicourse.py
+++ tests/test_data/test_tddicourse.py
@@ -8,7 +8,15 @@ def test_load_tddiscourse_train():
 
     assert isinstance(trainset, Dataset)
     assert set(trainset.column_names) == {"text", "label"}
-    assert len(trainset) == 4_650
+    assert len(trainset) == 4_000
+
+
+def test_load_tddiscourse_valid():
+    validset = load_tddiscourse("valid")
+
+    assert isinstance(validset, Dataset)
+    assert set(validset.column_names) == {"text", "label"}
+    assert len(validset) == 650
 
 
 def test_load_tddiscourse_test():
@@ -20,14 +28,18 @@ def test_load_tddiscourse_test():
 
 
 def test_load_dataset_tddiscourse():
-    testset = load_dataset("tddiscourse", "test")
     trainset = load_dataset("tddiscourse", "train")
+    validset = load_dataset("tddiscourse", "valid")
+    testset = load_dataset("tddiscourse", "test")
 
-    assert isinstance(testset, Dataset)
     assert isinstance(trainset, Dataset)
+    assert isinstance(validset, Dataset)
+    assert isinstance(testset, Dataset)
 
-    assert set(testset.column_names) == {"text", "label"}
     assert set(trainset.column_names) == {"text", "label"}
+    assert set(validset.column_names) == {"text", "label"}
+    assert set(testset.column_names) == {"text", "label"}
 
+    assert len(trainset) == 4_000
+    assert len(validset) == 650
     assert len(testset) == 1_500
-    assert len(trainset) == 4_650
