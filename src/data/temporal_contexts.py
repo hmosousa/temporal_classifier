2330741da3ea32cd66a8ac71914585641a8d90a3
Hugo Sousa <hmosousa@gmail.com>
2025-01-17 16:54:29 +0000

    Fix temporal contexts loading

--- src/data/temporal_contexts.py
+++ src/data/temporal_contexts.py
@@ -5,6 +5,7 @@ import datasets
 
 def load_temporal_contexts(
     split: Literal["train", "valid", "test"],
+    **kwargs,
 ) -> datasets.Dataset:
     """Used to train classification models."""
     dataset = datasets.load_dataset("hugosousa/TemporalContexts", split=split)
