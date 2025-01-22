806b489ca1efed7c2ce5a114720e1e0071978510
Hugo Sousa <hmosousa@gmail.com>
2025-01-22 10:10:08 +0000

    Implement correct metrics

--- scripts/eval/point.py
+++ scripts/eval/point.py
@@ -8,7 +8,7 @@ from sklearn.metrics import classification_report
 from src.base import RELATIONS
 from src.constants import RESULTS_DIR
 from src.data import load_dataset
-from src.metrics import compute_confidence_intervals
+from src.metrics import compute_confidence_intervals, compute_metrics
 from src.model import load_model, MajorityClassifier, RandomClassifier
 
 logging.basicConfig(level=logging.INFO)
@@ -34,12 +34,16 @@ def add_text_type(example: dict):
 
 
 def main(
-    model_name: str = "random",
+    model_name: str = "majority",
     revision: str = "main",
-    dataset_name: Literal["temporal_questions", "timeset", "matres"] = "matres",
-    config_name: str = None,
+    dataset_name: Literal[
+        "point_tempeval",
+        "timeset",
+        "matres",
+        "point_tddiscourse",
+        "point_timebank_dense",
+    ] = "timeset",
     batch_size: int = 32,
-    verbose: bool = False,
     confidence: bool = True,
 ):
     """Evaluate a model with a given configuration.
@@ -48,21 +52,19 @@ def main(
         model_name: The HuggingFace name of the model to evaluate.
         revision: The revision of the model to evaluate.
         dataset_name: The name of the dataset to evaluate on.
+        config_name: The name of the config to evaluate on.
+        batch_size: The batch size to use for evaluation.
         confidence: Whether to calculate confidence intervals.
     """
-    if dataset_name == "temporal_questions" and config_name is None:
-        config_name = "raw"
-
     logging.info("Running eval with the following parameters:")
     logging.info(f"  model_name: {model_name}")
     logging.info(f"  revision: {revision}")
     logging.info(f"  dataset_name: {dataset_name}")
-    logging.info(f"  config_name: {config_name}")
     logging.info(f"  batch_size: {batch_size}")
-    logging.info(f"  verbose: {verbose}")
+    logging.info(f"  confidence: {confidence}")
 
     logging.info(f"Loading dataset {dataset_name}")
-    dataset = load_dataset(dataset_name, split="test", config=config_name)
+    dataset = load_dataset(dataset_name, split="test")
     dataset = dataset.map(add_text_type)
 
     logging.info(f"Loading model {model_name}")
@@ -78,51 +80,48 @@ def main(
     preds = [p["label"] for p in preds]
     dataset = dataset.add_column("pred", preds)
 
-    logging.info("Calculating metrics for all text types")
-    report = classification_report(
+    logging.info("Calculating metrics")
+    metrics = compute_metrics(dataset["label"], dataset["pred"], labels=RELATIONS)
+    if confidence:
+        metrics["confidence"] = compute_confidence_intervals(
+            dataset["label"], dataset["pred"], labels=RELATIONS
+        )
+
+    logging.info("Calculating metrics for each label")
+    per_label = classification_report(
         y_true=dataset["label"],
         y_pred=dataset["pred"],
         output_dict=True,
         zero_division=0.0,
         labels=RELATIONS,
     )
-
-    if confidence:
-        report["confidence"] = compute_confidence_intervals(dataset, labels=RELATIONS)
-    if verbose:
-        print(classification_report(dataset["label"], dataset["pred"], digits=4))
+    per_label.pop("accuracy", None)
+    per_label.pop("micro avg", None)
+    per_label.pop("macro avg", None)
+    per_label.pop("weighted avg", None)
+    metrics["per_label"] = per_label
 
     logging.info("Calculating metrics for each text type")
-    report["type"] = {}
-    for text_type in TEXT_TYPES:
+    type_metrics = {}
+    dataset_text_types = list(set(dataset["type"]))
+    dataset_text_types.sort()
+    for text_type in dataset_text_types:
         dataset_type = dataset.filter(lambda x: x["type"] == text_type)
-        report_type = classification_report(
-            y_true=dataset_type["label"],
-            y_pred=dataset_type["pred"],
-            output_dict=True,
-            zero_division=0.0,
-            labels=RELATIONS,
+        type_metrics[text_type] = compute_metrics(
+            dataset_type["label"], dataset_type["pred"], labels=RELATIONS
         )
-        if verbose:
-            print(
-                classification_report(
-                    dataset_type["label"], dataset_type["pred"], digits=4
-                )
+        if confidence:
+            type_metrics[text_type]["confidence"] = compute_confidence_intervals(
+                dataset_type["label"], dataset_type["pred"], labels=RELATIONS
             )
-
-        report[text_type] = report_type
+    metrics["pre_type"] = type_metrics
 
     logging.info("Saving results")
     model_id = model_name.split("/")[-1]
-    if config_name:
-        outpath = (
-            RESULTS_DIR / "point" / f"{dataset_name}_{config_name}" / f"{model_id}.json"
-        )
-    else:
-        outpath = RESULTS_DIR / "point" / dataset_name / f"{model_id}.json"
+    outpath = RESULTS_DIR / "point" / dataset_name / f"{model_id}.json"
     outpath.parent.mkdir(parents=True, exist_ok=True)
     with open(outpath, "w") as f:
-        json.dump(report, f, indent=4)
+        json.dump(metrics, f, indent=4)
 
 
 if __name__ == "__main__":
--- src/metrics.py
+++ src/metrics.py
@@ -1,93 +1,96 @@
-import datasets
+import random
+
 import numpy as np
+import sklearn
 import torch.nn.functional as F
-from sklearn.metrics import classification_report
+
+
+def compute_metrics(
+    y_true,
+    y_pred,
+    labels=None,
+    zero_division=0.0,
+):
+    # Compute the metrics as in https://cogcomp.seas.upenn.edu/papers/NingSuRo19.pdf
+    if labels[-1] != "-":
+        raise ValueError(
+            f"The last label is {labels[-1]} not '-' (none relation) but it would be treated as none relation. This method is only for the case where the last label is none relation."
+        )
+
+    confusion_matrix = sklearn.metrics.confusion_matrix(
+        y_true, y_pred, labels=labels
+    )  # rows are true, columns are pred
+    if confusion_matrix.sum().item() == 0:
+        accuracy = zero_division
+    else:
+        accuracy = (confusion_matrix.diagonal().sum() / confusion_matrix.sum()).item()
+
+    pred_cm = confusion_matrix[:, :3]
+    if pred_cm.sum().item() == 0:
+        precision = zero_division
+    else:
+        precision = (pred_cm.diagonal().sum() / pred_cm.sum()).item()
+
+    true_cm = confusion_matrix[:3, :]
+    if true_cm.sum().item() == 0:
+        recall = zero_division
+    else:
+        recall = (true_cm.diagonal().sum() / true_cm.sum()).item()
+
+    if precision + recall == 0:
+        f1 = zero_division
+    else:
+        f1 = 2 * precision * recall / (precision + recall)
+
+    return {
+        "accuracy": accuracy,
+        "precision": precision,
+        "recall": recall,
+        "f1-score": f1,
+    }
 
 
 def compute_confidence_intervals(
-    dataset: datasets.Dataset, n_trials: int = 1_000, labels: list[str] = None
+    y_true, y_pred, n_trials: int = 1_000, labels: list[str] = None
 ):
     """Compute confidence intervals with bootstrap
     Args:
-        dataset: The dataset to compute the confidence intervals for. Assumes the dataset has "label" and "pred" columns.
+        y_true: The true labels.
+        y_pred: The predicted labels.
         n_trials: The number of trials to run.
         labels: The labels to compute the confidence intervals for.
     """
-    reports = {
-        "accuracy": [],
-        "macro avg": {
-            "precision": [],
-            "recall": [],
-            "f1-score": [],
-        },
-        "weighted avg": {
-            "precision": [],
-            "recall": [],
-            "f1-score": [],
-        },
-    }
+    metrics = {"accuracy": [], "precision": [], "recall": [], "f1-score": []}
     for _ in range(n_trials):
         # Randomly sample with replacement
-        sample = dataset.select(
-            np.random.choice(len(dataset), size=len(dataset), replace=True)
-        )
+        sample = random.choices(list(zip(y_true, y_pred)), k=len(y_true))
+        sample_y_true, sample_y_pred = zip(*sample)
+
         # Compute metrics
-        report_trial = classification_report(
-            y_true=sample["label"],
-            y_pred=sample["pred"],
-            output_dict=True,
-            zero_division=0.0,
+        sample_metrics = compute_metrics(
+            y_true=sample_y_true,
+            y_pred=sample_y_pred,
             labels=labels,
         )
-        reports["accuracy"].append(report_trial["accuracy"])
-        reports["macro avg"]["precision"].append(report_trial["macro avg"]["precision"])
-        reports["macro avg"]["recall"].append(report_trial["macro avg"]["recall"])
-        reports["macro avg"]["f1-score"].append(report_trial["macro avg"]["f1-score"])
-        reports["weighted avg"]["precision"].append(
-            report_trial["weighted avg"]["precision"]
-        )
-        reports["weighted avg"]["recall"].append(report_trial["weighted avg"]["recall"])
-        reports["weighted avg"]["f1-score"].append(
-            report_trial["weighted avg"]["f1-score"]
-        )
+        for metric, value in sample_metrics.items():
+            metrics[metric].append(value)
+
     confidence_intervals = {
         "accuracy": {
-            "lower": np.percentile(reports["accuracy"], 2.5).item(),
-            "upper": np.percentile(reports["accuracy"], 97.5).item(),
+            "lower": np.percentile(metrics["accuracy"], 2.5).item(),
+            "upper": np.percentile(metrics["accuracy"], 97.5).item(),
         },
-        "macro avg": {
-            "precision": {
-                "lower": np.percentile(reports["macro avg"]["precision"], 2.5).item(),
-                "upper": np.percentile(reports["macro avg"]["precision"], 97.5).item(),
-            },
-            "recall": {
-                "lower": np.percentile(reports["macro avg"]["recall"], 2.5).item(),
-                "upper": np.percentile(reports["macro avg"]["recall"], 97.5).item(),
-            },
-            "f1-score": {
-                "lower": np.percentile(reports["macro avg"]["f1-score"], 2.5).item(),
-                "upper": np.percentile(reports["macro avg"]["f1-score"], 97.5).item(),
-            },
+        "precision": {
+            "lower": np.percentile(metrics["precision"], 2.5).item(),
+            "upper": np.percentile(metrics["precision"], 97.5).item(),
         },
-        "weighted avg": {
-            "precision": {
-                "lower": np.percentile(
-                    reports["weighted avg"]["precision"], 2.5
-                ).item(),
-                "upper": np.percentile(
-                    reports["weighted avg"]["precision"], 97.5
-                ).item(),
-            },
-            "recall": {
-                "lower": np.percentile(reports["weighted avg"]["recall"], 2.5).item(),
-                "upper": np.percentile(reports["weighted avg"]["recall"], 97.5).item(),
-            },
-            "f1-score": {
-                "lower": np.percentile(reports["weighted avg"]["f1-score"], 2.5).item(),
-                "upper": np.percentile(
-                    reports["weighted avg"]["f1-score"], 97.5
-                ).item(),
-            },
+        "recall": {
+            "lower": np.percentile(metrics["recall"], 2.5).item(),
+            "upper": np.percentile(metrics["recall"], 97.5).item(),
+        },
+        "f1-score": {
+            "lower": np.percentile(metrics["f1-score"], 2.5).item(),
+            "upper": np.percentile(metrics["f1-score"], 97.5).item(),
         },
     }
     return confidence_intervals
